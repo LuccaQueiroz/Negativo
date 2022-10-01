223ae6c859fc2f5553319c89edbd109a71e2d174
Lucas <lucasqueirozzwerner@gmail.com>
2022-09-30 22:46:53 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.WriteLine("Você digitou um número negativo");
+
+}
+
+else 
+{
+    Console.WriteLine("Você digitou um número positivo");
+
+}
